6b17388be3c7fcdf4708fe36dc12344d7393f4b6
DenisBorisov-lab <mai.borisov.denis@gmail.com>
2022-05-10 16:29:07 +0300

    message

---
+++ src/main/java/com/example/yandexspringpull/YandexSpringPullApplication.java
@@ -0,0 +1,13 @@
+package com.example.yandexspringpull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexSpringPullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexSpringPullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yandexspringpull/YandexSpringPullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yandexspringpull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexSpringPullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
